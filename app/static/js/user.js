55d9886e3b5e2826687c19c445f0595263d66c4d
Perry Wang <perrywangsh@gmail.com>
2015-12-22 19:40:37 +0800

    add user socketio support to listener answer_added event

--- app/static/js/user.js
+++ app/static/js/user.js
@@ -1,3 +1,10 @@
 $(function(){
 	$('select').dropdown();
 });
+var socket = io.connect('http://' + document.domain + ':' + location.port + '/users');
+socket.on('connect', function(data) {
+    console.log('hello');
+    socket.on('answer_added',function(data){
+         console.log(data);
+    });
+});
